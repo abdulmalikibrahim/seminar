d04d9123d32c34816894c26b7b556252203b7f77
Abdul Malik Ibrahim <45823670+abdulmalikibrahim@users.noreply.github.com>
2025-06-06 21:01:25 +0700

    Update LinkGroupWA.jsx

--- frontend/src/component/LinkGroupWA.jsx
+++ frontend/src/component/LinkGroupWA.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LinkGroupWA = () => {
     return(
-        <a href="https://chat.whatsapp.com/FQADHb7XBIrD6KAYmoztC7" target='_blank' className='btn btn-success'>
+        <a href="https://chat.whatsapp.com/E6ditA2GmMx7i9ugJUea7R" target='_blank' className='btn btn-success'>
             <img src="https://www.tanjunglesung.com/wp-content/uploads/2018/12/logo-wa-whatsapp-300x300.png" width="30" />
             <span className='ml-2'>&nbsp;Group WA</span>
         </a>
